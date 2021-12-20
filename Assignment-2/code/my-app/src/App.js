c3d14f540274d053c45f7db1f056abe4e7bce3b9
vipin kumar <kumarvipin24031999@gmail.com>
2021-12-20 10:55:42 +0530

    assignment-2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,18 +1,64 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-
-
-function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+function Userinfo(props)
+{
+  return (
+    <div className = "userProfile">
+    <img src = {props.employee.profileImg} 
+    alt = {props.employee.name}/>
+  </div>
+  );
 }
 
+function Username(props)
+{
+  return (
+    <div className = "userName">
+    <h1>{props.employee.name}</h1>
+  </div>
+  );
+}
 
+function Location(props)
+{
+  return (
+    <div className = "location">
+    <p>Location</p>
+    <h1>{props.employee.location}</h1>
+  </div>
+  );
+}
+
+function Bloodgroup(props)
+{
+  return (
+    <div className = "bloodGroup">
+        <p>Blood Group</p>
+        <h1>{props.employee.bloodGroup}</h1>
+      </div>
+  );
+}
+
+function Age(props)
+{
+  return (
+    <div className = "age">
+        <p>Age</p>
+        <h1>{props.employee.age}</h1>
+      </div>
+  );
+}
+function App(props) {
+  return (
+    <div className = "app">
+      <Userinfo employee = {props.employee}/>
+      <Username employee = {props.employee}/>
+      <Location employee = {props.employee}/>
+      <Bloodgroup employee = {props.employee}/>
+      <Age employee = {props.employee}/>
+    </div>
+  );
+}
 
 export default App;
--- Assignment-2/code/my-app/src/App.test.js
+++ Assignment-2/code/my-app/src/App.test.js
@@ -1,20 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import Enzyme from "enzyme";
 import App from './App';
 
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-const { shallow } = Enzyme;
-Enzyme.configure({ adapter: new Adapter() })
-
-test("renders correctly", () => {
- 
-const employee = {
-  name: 'Tom',
-  location: 'New York',
-  bloodGroup: 'B+',
-  age: 28,
-profileImg: 'https://www.w3schools.com/howto/img_avatar.png'
-}
-  shallow(<App employee={employee} />);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
